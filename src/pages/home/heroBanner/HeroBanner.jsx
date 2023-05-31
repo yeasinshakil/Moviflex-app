a29b30162946a2b1055dfb3c09a454862765d269
yeasinshakil <shakilrock1998@gmail.com>
2023-05-31 16:25:01 +0600

    home hero section dynamic banner update

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './heroBanner.scss'
-import batmanPoster from '../../../assets/batman poster.png'
+// import batmanPoster from '../../../assets/batman poster.png'
 import Img from '../../../components/lazyLoadImage/LazyLoadImage'
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import { useNavigate } from 'react-router-dom'
+import useFetch from '../../../components/hooks/useFetch'
+import { useSelector } from 'react-redux'
 
 function HeroBanner() {
+    const [background, setBackground] = useState('');
+    const [query, setQuery] = useState('');
+    const navigate = useNavigate();
+
+    const { data, loading } = useFetch('/movie/upcoming');
+
+    const { url } = useSelector(state => state.home)
+    useEffect(() => {
+        const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+        setBackground(bg)
+    }, [data])
+    const searchQueryHandler = (event) => {
+        if (event.key === 'Enter' && query.length > 0) {
+            navigate(`/search/${query}`)
+        }
+    }
     return (
         <div className=' w-full h-[450px] md:h-[700px] bg-black1 flex items-center relative'>
             <div className=' w-full h-full absolute top-0 left-0 opacity-50 overflow-hidden'>
-                <Img src={batmanPoster} alt="" />
+                <Img src={background} alt="" />
             </div>
             <div className=' w-full h-[250px] absolute bottom-0 left-0 bg-gradient-to-b from-transparent to-black1'></div>
             <ContentWrapper>
@@ -16,8 +35,13 @@ function HeroBanner() {
                     <span className=' text-[50px] md:text-[90px] md:mb-0 font-bold mb-[10px]'>Welcom.</span>
                     <span className=' text-[18px] mb-[40px] font-medium md:text-[24px]'>Millions of Movies, Tv shows and lot's of people to discover. Explore Now</span>
                     <div className=' w-full flex items-center'>
-                        <input type="text" placeholder='Search for a movie or tv show.....' className=' w-5/6 h-[50px] bg-white border-0 outline-none rounded-l-[30px] py-0 px-[20px] text-[14px] md:w-4.5/6 md:h-[60px] md:text-[20px] md:px-[30px]'/>
-                        <button className=' w-[100px] md:w-[150px] text-[16px] md:text-[18px] text-white h-[50px] md:h-[60px] outline-none border-none cursor-pointer rounded-r-[30px] bg-gradient-to-r from-yellow-500 to-pink1 hover:bg-gradient-to-l transition duration-0 hover:transition-all hover:duration-1000'>Search</button>
+                        <input type="text" placeholder='Search for a movie or tv show.....'
+                            className=' w-5/6 h-[50px] bg-white border-0 outline-none rounded-l-[30px] py-0 px-[20px] text-[14px] md:w-4.5/6 md:h-[60px] md:text-[20px] md:px-[30px]'
+                            onChange={(e) => setQuery(e.target.value)}
+                            onKeyUp={searchQueryHandler} />
+                        <button 
+                        className=' w-[100px] md:w-[150px] text-[16px] md:text-[18px] text-white h-[50px] md:h-[60px] outline-none border-none cursor-pointer rounded-r-[30px] bg-gradient-to-r from-yellow-500 to-pink1 hover:bg-gradient-to-l transition duration-0 hover:transition-all hover:duration-1000'
+                        >Search</button>
                     </div>
                 </div>
             </ContentWrapper>
