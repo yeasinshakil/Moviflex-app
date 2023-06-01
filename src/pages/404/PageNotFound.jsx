23315e79eaf7666e0a30ec76960a3709ded73887
yeasinshakil <shakilrock1998@gmail.com>
2023-06-01 16:54:43 +0600

    page not found setup

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-function PageNotFound() {
+// import "./pageNotFound.scss";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+
+const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
-}
+    <div className=" h-[700px] pt-[200px]">
+      <ContentWrapper>
+        <div className=" text-center text-blackLight flex flex-col">
+          <span className=" text-[150px] font-bold">404</span>
+          <span className=" text-5xl">Page not found!</span>
+        </div>
 
-export default PageNotFound
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default PageNotFound;
